7239032e655a1c98c76c03dc2c90d011c636cb56
Sunny-Sahota <sunny.sahota989@gmail.com>
2026-09-16 15:48:23 +0100

    CI Fix , Test Id more unique
    Previous CI failures due to ID uniqueness based on the same Id from Date Time

--- PetStoreTests/Helpers/TestDataFactory.cs
+++ PetStoreTests/Helpers/TestDataFactory.cs
@@ -4,12 +4,13 @@ namespace PetStoreTests.Helpers
 {
     public class TestDataFactory
     {
-        // Test data creation
+        //  Test data creation
+        //  Note: I had to make Id more unique because the tests run in parallel so the same id was being used based on Date time
         public static Pet CreatePet()
         {
             return new Pet
             {
-                Id = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds(),
+                Id = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() * 1000 + Random.Shared.Next(0, 999),
                 Name = "TestPet",
                 Status = "available"
             };
